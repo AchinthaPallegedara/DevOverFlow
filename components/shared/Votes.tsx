ea8984433ff109dc3cd0883f7b034b2844046ba3
Achintha Pallegedara <achinthahirudika@gmail.com>
2024-07-06 20:54:31 +0530

    loading and toast added

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -11,6 +11,7 @@ import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { toast } from "../ui/use-toast";
 
 interface Props {
   type: string;
@@ -42,11 +43,20 @@ const Votes = ({
       questionId: JSON.parse(itemId),
       path: pathname,
     });
+    toast({
+      title: `Question ${
+        !hasSaved ? "Saved in" : "removed from"
+      } your collection`,
+      variant: !hasSaved ? "default" : "destructive",
+    });
   };
 
   const handleVote = async (action: string) => {
     if (!userId) {
-      return;
+      return toast({
+        title: "Please login in",
+        description: "You must be logged in to perform this action",
+      });
     }
 
     if (action === "upvote") {
@@ -69,7 +79,10 @@ const Votes = ({
       }
 
       // todo: show a toast
-      return;
+      return toast({
+        title: `Upvote ${!hasupVoted ? "successful" : "removed"}`,
+        variant: !hasupVoted ? "default" : "destructive",
+      });
     }
 
     if (action === "downvote") {
@@ -91,7 +104,10 @@ const Votes = ({
         });
       }
 
-      // todo: show a toast
+      toast({
+        title: `Downvote ${!hasupVoted ? "successful" : "removed"}`,
+        variant: !hasupVoted ? "default" : "destructive",
+      });
     }
   };
 
